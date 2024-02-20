a1b6918f4d4ae90aa735185b032376ddbdca5089
Derek Antrican <derekantrican@gmail.com>
2024-02-19 21:17:58 -0800

    Set up some initial redirects

---
+++ redirects.js
@@ -0,0 +1,10 @@
+const redirects = [
+  {
+    pathname: '/halal',
+    target: 'https://derekantrican.com/halal',
+  },
+  {
+    pathname: '/turkish',
+    target: 'https://derekantrican.com/turkish',
+  },
+];
