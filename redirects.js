00acf367449f84a06a08c3d0809b32535045f2b9
Derek Antrican <derekantrican@gmail.com>
2024-03-03 21:39:04 -0800

    Added iftar redirect

--- redirects.js
+++ redirects.js
@@ -7,4 +7,8 @@ const redirects = [
     pathname: '/turkish',
     target: 'https://derekantrican.com/turkish',
   },
+  {
+    pathname: '/iftar',
+    target: 'https://www.eventbrite.com/e/interfaith-iftar-dinner-at-eastside-church-tickets-845508327167',
+  },
 ];
