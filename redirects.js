9c3f8775972c6308a135a5182b8b8f920b8c9132
Derek Antrican <derekantrican@gmail.com>
2024-04-17 09:04:09 -0700

    Added camping shortcut

--- redirects.js
+++ redirects.js
@@ -11,4 +11,8 @@ const redirects = [
     pathname: '/iftar',
     target: 'https://www.eventbrite.com/e/interfaith-iftar-dinner-at-eastside-church-tickets-845508327167',
   },
+  {
+     pathname: '/camping',
+     target: 'https://docs.google.com/document/d/1K1ypKhfrDPNBqTugY-3fFofOGmNTJyeG_Y7gwS72Bzc/edit?usp=drivesdk',
+  },
 ];
